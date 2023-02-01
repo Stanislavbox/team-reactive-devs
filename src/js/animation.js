83c8ee232f85b80d63230dcdcf5d951ccd2369df
Utilizator <Stanislavbox24@gmail.com>
2023-02-01 14:21:35 +0200

    add

--- src/js/animation.js
+++ src/js/animation.js
@@ -9,7 +9,19 @@ let options = {
   threshold: [0.5],
 };
 let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.restaurant-section-title');
-for (let elm of elements) {
+let elements1 = document.querySelectorAll('.restaurant-section-title');
+let elements2 = document.querySelectorAll('.title-room');
+let elements3 = document.querySelectorAll('.hotel-room-two-title');
+let elements4 = document.querySelectorAll('.hotel-gallery-triple-title');
+for (let elm of elements1) {
+  observer.observe(elm);
+}
+for (let elm of elements2) {
+  observer.observe(elm);
+}
+for (let elm of elements3) {
+  observer.observe(elm);
+}
+for (let elm of elements4) {
   observer.observe(elm);
 }
