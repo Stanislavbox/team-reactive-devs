09148f64e9c144df30fc51b3e5c32a64a3d2deb8
Stanislavbox <Stanislavbox24@gmail.com>
2023-02-03 09:56:09 +0000

    Deploying to gh-pages from @ Stanislavbox/team-reactive-devs@77032662d750989085f8b9f085a0448a11204bad 🚀

--- index.0372b5e1.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".hp-restaurant-slider",{slidesPerView:1,spaceBetween:46,slideToClickedSlide:!0,loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},breakpoints:{414:{slidesPerView:1.5},1024:{spaceBetween:34,slidesPerView:3},1280:{spaceBetween:87,slidesPerView:3}}});
-//# sourceMappingURL=index.0372b5e1.js.map
